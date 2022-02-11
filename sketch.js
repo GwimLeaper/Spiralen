62f0926f262bdf7621746b5e8ef09d31bd092b29
GwimLeaper <94458536+GwimLeaper@users.noreply.github.com>
2022-02-11 08:15:30 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+function setup() {
+  createCanvas(400, 400, WEBGL);
+  angleMode(DEGREES);
+}
+
+function draw() {
+  background(30);
+
+  rotateX(65)
+
+  noFill();
+  stroke(255);
+
+  for (var i = 0; i < 20; i++) {
+    var r = map(sin(frameCount), -1, 1, 0, 255);
+    var g = map(i, 0, 20, 0, 255);
+    var b = map(cos(frameCount), -1, 1, 255, 0);
+    
+    stroke(r, g, b)
+    beginShape();
+    for (var j = 0; j < 170; j += 10) {
+      var rad = i * 8;
+      var x = rad * cos(j);
+      var y = rad * sin(j);
+      var z = sin(frameCount * 2 + i * 10) * 50;
+
+      vertex(x, y, z);
+    }
+    endShape(CLOSE);
+  }
+}
